467d9b5d1f9c0dd50b16dce21779a5ac591180ad
Rodrigo <rodrigo.pimpao94@gmail.com>
2021-01-04 21:23:20 -0300

    Implement Products controller, Service and TypeORM repository.

--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -21,21 +21,61 @@ class ProductsRepository implements IProductsRepository {
     price,
     quantity,
   }: ICreateProductDTO): Promise<Product> {
-    // TODO
+    const product = this.ormRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await this.ormRepository.save(product);
+
+    return product;
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
-    // TODO
+    const product = await this.ormRepository.findOne({ where: { name } });
+
+    return product;
   }
 
   public async findAllById(products: IFindProducts[]): Promise<Product[]> {
-    // TODO
+    const uniqueIds = products.reduce<string[]>((acc, curr) => {
+      if (acc.indexOf(curr.id) === -1) {
+        acc.push(curr.id);
+      }
+      return acc;
+    }, []);
+
+    const foundProducts = await this.ormRepository.find({
+      where: { id: In(uniqueIds) },
+    });
+
+    return foundProducts;
   }
 
   public async updateQuantity(
     products: IUpdateProductsQuantityDTO[],
   ): Promise<Product[]> {
-    // TODO
+    const productIds = products.map(product => product.id);
+
+    const foundProducts = await this.ormRepository.find({
+      where: { id: In(productIds) },
+    });
+
+    const updatedProducts = foundProducts.map(product => {
+      const orderProduct = products.find(prod => prod.id === product.id);
+
+      // Create a copy as TS complains about property reassigning
+      const updatedProduct = { ...product };
+
+      updatedProduct.quantity -= orderProduct?.quantity || 0;
+
+      return updatedProduct;
+    });
+
+    await this.ormRepository.save(updatedProducts);
+
+    return updatedProducts;
   }
 }
 
