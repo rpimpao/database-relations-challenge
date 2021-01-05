209ae46f94c832d996d671a93d5c743b6d7a0f06
Rodrigo <rodrigo.pimpao94@gmail.com>
2021-01-04 21:23:48 -0300

    Implement Orders Controller, Services and TypeORM repository.

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,61 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('Customer doesnt exist.');
+    }
+
+    const existingProducts = await this.productsRepository.findAllById(
+      products,
+    );
+
+    if (products.length > existingProducts.length) {
+      throw new AppError('Invalid product cannot be added to your order.');
+    }
+
+    existingProducts.forEach(product => {
+      const orderProduct = products.find(prod => prod.id === product.id);
+
+      if (orderProduct && orderProduct.quantity > product.quantity) {
+        throw new AppError('Not enough stock to fulfill your order');
+      }
+    });
+
+    const orderProducts = products.map(product => {
+      const existingProduct = existingProducts.find(
+        prod => prod.id === product.id,
+      );
+
+      const productPrice = (existingProduct && existingProduct.price) || 1;
+
+      return {
+        product_id: product.id,
+        price: productPrice,
+        quantity: product.quantity,
+      };
+    });
+
+    const order = await this.ordersRepository.create({
+      customer,
+      products: orderProducts,
+    });
+
+    await this.productsRepository.updateQuantity(products);
+
+    return order;
   }
 }
 
